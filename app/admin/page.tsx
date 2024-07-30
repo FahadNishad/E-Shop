4232f416dd34b1306333dee732c248edab894a4c
FahadNishad <fahadnishad1122@gmail.com>
2024-07-30 22:28:43 +0500

    update

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,15 +4,22 @@ import getProducts from "@/actions/getProducts";
 import getOrders from "@/actions/getOrders";
 import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
+import BarGraph from "./BarGraph";
+import getGraphData from "@/actions/getGrapghData";
 
 const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
+
+  const graphData = await getGraphData();
   return (
     <div className="pt-6">
       <Container>
         <Summary products={products} orders={orders} users={users} />
+        <div className="mt-4 mx-auto max-w-[1150px]:">
+          <BarGraph data={graphData} />
+        </div>
       </Container>
     </div>
   );
